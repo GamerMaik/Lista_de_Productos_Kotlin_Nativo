4be0bb09b43defac9a9d2b35a1feecada84b28f7
GamerMaik <gammermaikyt@gmail.com>
2023-05-16 02:49:01 -0400

    Funcionalidad de botones aumenta y disminuir Cantidad Agregada

--- app/src/main/java/com/example/modulopedidospollossilver/ProductDetailActivity.kt
+++ app/src/main/java/com/example/modulopedidospollossilver/ProductDetailActivity.kt
@@ -2,14 +2,47 @@ package com.example.modulopedidospollossilver
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.modulopedidospollossilver.databinding.ActivityProductDetailBinding
 
 class ProductDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProductDetailBinding
+    private var cantidad = 1;
+    private var estado=false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityProductDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        CargarDatos()
+        binding.textCantidadProduct.text= "Cantidad: $cantidad"
+        binding.BtnAgregar.setOnClickListener {
+            Toast.makeText(this,"Producto Agregado",Toast.LENGTH_SHORT).show()
+        }
+        binding.BtnMenos.setOnClickListener {
+            RestarCantidad()
+        }
+        binding.BtnMas.setOnClickListener {
+            SumarCantidad()
+        }
+
+    }
+    fun SumarCantidad(){
+        if (cantidad<5){
+            cantidad +=1
+            binding.textCantidadProduct.text= "Cantidad: $cantidad"
+        }else{
+            Toast.makeText(this,"Maxima cantidad del Producto",Toast.LENGTH_SHORT).show()
+        }
+    }
+    fun RestarCantidad(){
+        if (cantidad >1){
+            cantidad -=1
+            binding.textCantidadProduct.text= "Cantidad: $cantidad"
+        }else{
+            Toast.makeText(this,"Minima cantidad del Producto",Toast.LENGTH_SHORT).show()
+        }
+    }
+    fun CargarDatos(){
         val imgId = intent.getIntExtra("imgResID",0)
         val productName = intent.getStringExtra("productName")
         val productPrice = intent.getStringExtra("productPrice")
